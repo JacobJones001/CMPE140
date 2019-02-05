3f997ccc4cfe1982c51468ad030776fb08a15956
Jacob Jones <jacobjones@Jacobs-MacBook-Pro.local>
2019-02-05 13:55:31 -0800

    Added structure for factorial design and testing

---
+++ Lab1/src/factorial.v
@@ -0,0 +1,13 @@
+module factorial #(parameter DATA_WIDTH = 32)(
+    input rst, clk,
+    input Go,
+    input [DATA_WIDTH-1:0] n,
+    output Done, Error
+    output [DATA_WIDTH-1:0] product
+);
+
+
+
+
+
+endmodule
--- Lab1/src/tb_factorial.v
+++ Lab1/src/tb_factorial.v
@@ -1,9 +1,22 @@
 `timescale 1ns / 1ps
 
 
-module tb_factorial #(parameter DATA_WIDTH = 4);
+module tb_factorial #(parameter DATA_WIDTH = 32);
     reg clk_tb, rst_tb;
+    reg Go_tb;
+    reg [DATA_WIDTH-1:0] n_tb;
+    wire Done_tb, Error_tb;
+    wire [DATA_WIDTH-1:0] product_tb;
 
+    factorial #(.DATA_WIDTH=DATA_WIDTH) DUT(
+        .clk(clk_tb),
+        .rst(rst_tb),
+        .Go(Go_tb),
+        .n(n_tb),
+        .Done(Done_tb),
+        .Error(Error_tb),
+        .product(product_tb)
+    );
 
 
 endmodule
