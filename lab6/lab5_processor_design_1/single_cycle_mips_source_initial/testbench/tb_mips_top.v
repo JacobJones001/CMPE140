a19b0db4498262ca2077351d5b576062cc805d47
Jacob Jones <jacobjones@Jacobs-MacBook-Pro.local>
2019-04-11 15:35:52 -0700

    Created skeleton for testbench

--- lab6/lab5_processor_design_1/single_cycle_mips_source_initial/testbench/tb_mips_top.v
+++ lab6/lab5_processor_design_1/single_cycle_mips_source_initial/testbench/tb_mips_top.v
@@ -28,7 +28,16 @@ module tb_mips_top;
     // wire [31:0] shift_rd1_mux_a = DUT.mips.dp.shift_rd1_mux.a;
     // wire [31:0] shift_rd1_mux_b = DUT.mips.dp.shift_rd1_mux.b;
     // wire [31:0] shift_rd1_mux_y = DUT.mips.dp.shift_rd1_mux.y;
-    wire shift_mux_sel = DUT.mips.shift_mux_sel;
+    // wire pc_next = DUT.mips.pc_next;
+    wire [31:0] pc_next = DUT.mips.dp.pc_next;
+    wire [31:0] alu_out_hi = DUT.mips.dp.alu_out_hi;
+
+
+    integer error_count = 0;
+    integer a, b;
+    reg [63:0] expected_product;
+    integer expected_jmp_value; // JR and JAL check
+    integer reg1_value;
 
     
     mips_top DUT (
@@ -60,14 +69,109 @@ module tb_mips_top;
         rst = 1'b0;
     end
     endtask
+
+    task CHECK_MULTU;
+    begin
+        a = 2;
+        b = 4;
+        expected_product = a*b;
+        if({alu_out_hi, alu_mux_out} != expected_product) begin
+            error_count = error_count + 1;
+            $display("Error: MULTU - Product");
+        end 
+    end
+    endtask
+
+    task CHECK_MFHI;
+    begin
+        if(wd_rf[31:0] != expected_product[63:32]) begin
+            error_count = error_count + 1;
+            $display("Error: MFHI - Value");
+        end 
+    end
+    endtask
+
+    task CHECK_MFLO;
+    begin
+        if(wd_rf[31:0] != expected_product[31:0]) begin
+            error_count = error_count + 1;
+            $display("Error: MFLO - Value");
+        end 
+    end
+    endtask
+
+    task CHECK_JR;
+    begin
+        expected_jmp_value = 'h1c;
+        if(pc_next != expected_jmp_value) begin
+            error_count = error_count + 1;
+            $display("Error: CHECK_JR - Value");
+        end 
+    end
+    endtask
+
+    task CHECK_JAL;
+    begin
+        if(pc_next[27:2] != instr[25:0]) begin
+            error_count = error_count + 1;
+            $display("Error: CHECK_JAL - Value");
+        end 
+    end
+    endtask
+
+    task CHECK_SLL;
+    begin
+        reg1_value = 1;
+        if(alu_mux_out != (reg1_value << instr[10:6])) begin
+            error_count = error_count + 1;
+            $display("Error: CHECK_SLL - Value");
+        end 
+    end
+    endtask
+
+    task CHECK_SRL;
+    begin
+        reg1_value = 8;
+        if(alu_mux_out != (reg1_value >> instr[10:6])) begin
+            error_count = error_count + 1;
+            $display("Error: CHECK_SRL - Value");
+        end 
+    end
+    endtask
+
+    task run_test;
+    begin
+        tick;
+        tick;
+        tick;
+        CHECK_MULTU;
+        tick; 
+        CHECK_MFLO;
+        tick;
+        CHECK_JR;
+        tick;
+        CHECK_JAL;
+        tick; // addi
+        tick;
+        CHECK_SLL;
+        tick;
+        CHECK_SRL;
+        tick;
+        
+    end
+    endtask
+
     
     initial begin
         reset;
-        while(pc_current != 32'h48) 
-        begin
-            tick;
-        end
+        run_test;
+        // while(pc_current != 32'h48) 
+        // begin
+        //     tick;
+        // end
+        $display("Error Count: ", error_count);
         $finish;
     end
+
 
 endmodule
